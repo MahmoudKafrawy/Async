774f83f741cdd5b14d82f5859a26fcaf502e1b0c
Mahmoud Elkafrawy@ELKAFRAWY <mahmoud.kafrawy96@gmail.com>
2022-06-01 16:08:59 +0200

    he

--- script.js
+++ script.js
@@ -4,8 +4,47 @@ x.then((data) => {
   return data.json();
 }).then((user) => {
   for (var i = 0; i < user.length; i++) {
-    userEL = document.createElement("span");
+    userEL = document.createElement("button");
     userEL.innerHTML = user[i].name;
+    userEL.addEventListener("click", function () {
+      var y = fetch(`https://jsonplaceholder.typicode.com/posts?userId=${spans[i]}`);
+      async function t() {
+        try {
+          var ids = await getids;
+          console.log(ids);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      t();
+    });
     document.body.append(userEL);
   }
 });
+
+const buttons = document.getElementsByTagName("button");
+
+// for (i = 0; i < buttons.length; i++) {
+//   // buttons[i].addEventListener("Click", function () {
+//     // var y = fetch(`https://jsonplaceholder.typicode.com/posts?userId=${spans[i]}`);
+//     // y.then((data) => {
+//     //   console.log(data);
+//     // });
+
+//     // async function t() {
+//     //     try {
+//     //         var ids = await getids;
+//     //         console.log(ids);
+//     //         var user = await getUser(ids[2]);
+//     //         console.log(user);
+//     //         console.log(await getAddress(user.name));
+
+//     //     }
+//     //     catch (e) {
+//     //         console.log(e);
+
+//     //     }
+//     // }
+//     // t();
+//     // console.log(5);
+//   };

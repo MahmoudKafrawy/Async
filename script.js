bc4c670d86044b3b5a7c7285db8ef6b00c33aee8
Mostafa <mostafa.udemy1@gmail.com>
2022-06-01 20:22:39 +0200

    latest

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-var x = fetch("https://jsonplaceholder.typicode.com/users");
+try {
+  var x = fetch("https://jsonplaceholder.typicode.com/users");
+  x.then((data) => {
+    return data.json();
+  }).then((user) => {
+    for (let i = 0; i < user.length; i++) {
+      let userEL = document.createElement("button");
+      let container = document.getElementById("users");
+      userEL.innerText = user[i].name;
+      document.body.append(container);
+      container.append(userEL);
+      userEL.addEventListener("click", function (e) {
+        // console.log(e.target.innerText);
+        asyncFunction(i);
+      });
+    }
+  });
+} catch (error) {
+  console.log(error);
+}
 
-x.then((data) => {
-  return data.json();
-}).then((user) => {
-  for (var i = 0; i < user.length; i++) {
-    userEL = document.createElement("span");
-    userEL.innerHTML = user[i].name;
-    document.body.append(userEL);
+async function asyncFunction(i) {
+  try {
+    let x = await fetch(
+      "https://jsonplaceholder.typicode.com/posts?userId=" + (i + 1)
+    );
+    let y = await x.json();
+    console.log(y);
+    let z = document.getElementById("posts");
+    z.innerHTML = "";
+    y.forEach(function (post) {
+      let postEL = document.createElement("div");
+      postEL.innerHTML = post.title;
+      z.appendChild(postEL);
+    });
+  } catch (error) {
+    console.log(error);
   }
-});
-
+}
+// asyncFunction(0);

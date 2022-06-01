61131de68c74ad728a6e9c952d4006db858fb77e
Mahmoud Elkafrawy@ELKAFRAWY <mahmoud.kafrawy96@gmail.com>
2022-06-02 01:49:21 +0200

    m

--- script.js
+++ script.js
@@ -21,9 +21,7 @@ try {
 
 async function asyncFunction(i) {
   try {
-    let x = await fetch(
-      "https://jsonplaceholder.typicode.com/posts?userId=" + (i + 1)
-    );
+    let x = await fetch("https://jsonplaceholder.typicode.com/posts?userId=" + (i + 1));
     let y = await x.json();
     console.log(y);
     let z = document.getElementById("posts");
@@ -36,10 +34,5 @@ async function asyncFunction(i) {
   } catch (error) {
     console.log(error);
   }
-<<<<<<< HEAD
-  const span = document.getElementsByTagName("spans");
-});
-=======
 }
 // asyncFunction(0);
->>>>>>> bc4c670d86044b3b5a7c7285db8ef6b00c33aee8

0e682884bff3fc0645384787eb1c938b7d1d99c8
Mahmoud Elkafrawy@ELKAFRAWY <mahmoud.kafrawy96@gmail.com>
2022-06-01 14:30:42 +0200

    new

---
+++ script.js
@@ -0,0 +1,11 @@
+var x = fetch("https://jsonplaceholder.typicode.com/users");
+
+x.then((data) => {
+  return data.json();
+}).then((user) => {
+  for (var i = 0; i < user.length; i++) {
+    userEL = document.createElement("div");
+    userEL.innerHTML = user[i].name;
+    document.body.append(userEL);
+  }
+});
